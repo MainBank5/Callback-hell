dc495e69e42d7c5a5f5d11305465fd2622733d71
eliudkaruga <eliudkaruga97@gmail.com>
2023-09-08 00:32:12 +0300

    promises chaining

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ xhr.onreadystatechange = function () {
 
 xhr.send()*/
 
-
+console.log('Hello from global')
 
 
 
@@ -72,4 +72,67 @@ promise.then(() => {
     console.log('Promise was consumed')
 })
 
-console.log('Hello from global')
+const getUser = new Promise((resolve, reject) => {
+    //async function/task
+    setTimeout(()=>{
+        let error = false
+
+        if (!error) {
+          resolve({ name: "Karuga", age: 30})  
+        }
+        else {
+            reject("There is a big big Error")
+        }
+    }, 2000)
+})
+
+getUser.then((user) => {
+    console.log(user)
+}).catch((fallback) => {
+    console.warn(fallback)
+})
+
+/*.finally(() => {
+    console.log("with finally the code runs regardless of resolve or rejected status")
+})*/
+
+let p = new Promise ((resolve, reject) => {
+    let a = 1 + 1
+    if (a === 2) {
+        resolve('Promise sucess')
+    } else {
+        reject('Failed Promise')
+    }
+})
+
+p.then((message) => {
+    console.log('This is in the then ' + message)
+}).catch((message) => {
+    console.log("this is the catch " + message)
+})
+
+//you can run mutiple promises as one 
+const recordVideoOne = new Promise ((resolve, reject) => {
+    resolve("Video One was Recorded")
+});
+const recordVideoTwo = new Promise ((resolve, reject) => {
+    resolve("Video two was Recorded")
+});
+const recordVideoThree = new Promise ((resolve, reject) => {
+    resolve("Video three was Recorded")
+});
+
+
+/*promise.all([recordVideoOne, 
+recordVideoTwo, 
+recordVideoThree]).then((messages) => {
+    console.log(messages)
+})
+
+//as soon as the first one gets executed the message is returned 
+promise.race([recordVideoOne, 
+recordVideoTwo, 
+recordVideoThree]).then((message) => {
+    console.log(message)
+})*/
+
